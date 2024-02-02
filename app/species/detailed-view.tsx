c48c55be9529e4e1dfd8c5e1864021879e262807
Vivian Hui <vivianhui05@gmail.com>
2024-02-02 17:27:29 -0500

    created detailed-view component - not funcitioning

---
+++ app/species/detailed-view.tsx
@@ -0,0 +1,22 @@
+
+import { createServerSupabaseClient } from '@/lib/server-utils';
+import React from 'react'; 
+
+
+
+const DetailedView = () => { 
+
+ 
+  return ( 
+    
+    <div> 
+    
+      <h1>{"hi"}</h1> 
+      
+    </div> 
+    
+  ); 
+  
+}; 
+
+export {DetailedView};
